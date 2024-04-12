3f1095d3177e88d31c9748041427b7a161114a2b
Morgan Contrino <98786957+morgnan@users.noreply.github.com>
2024-04-12 11:58:09 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config(); // If using dotenv for environment variables
+const routes = require("./routes");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB:", err));
+
+app.use(express.json());
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
